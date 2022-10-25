f9da9d8af369a1d94888c6258549e1a9406ddc73
cf <316323626@qq.com>
2022-10-25 14:31:10 +0800

    Init basic and wx-server module

---
+++ wx-server/src/test/java/cn/autumn/wxserver/WxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.autumn.wxserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
